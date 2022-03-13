459f5c70899456cf2019d2fcbfb0d177684b2e9a
Louis Midson Lajeanty <louismidsonlajeanty@gmail.com>
2022-03-12 20:37:26 -0500

    Add Sources Files

---
+++ src/function.hpp
@@ -0,0 +1,28 @@
+/*
+ * function.hpp
+ *
+ *  Created on: Mar 9, 2022
+ *      Author: mds
+ */
+
+#ifndef FUNCTION_HPP_
+#define FUNCTION_HPP_
+
+void challenge_1(void);
+void challenge_2(void);
+void challenge_3(void);
+void challenge_4(void);
+void challenge_5(void);
+void challenge_6(void);
+void challenge_7(void);
+void challenge_8(void);
+void challenge_9(void);
+void challenge_10(void);
+void challenge_11(void);
+void challenge_12(void);
+void challenge_13(void);
+void challenge_14(void);
+void challenge_15(void);
+
+
+#endif /* FUNCTION_HPP_ */
---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : Louis Midson LAJEANTY
+// Version     : 1.0.0
+// Copyright   : Copyright - ESIH 2022
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include "function.hpp"
+
+int main() {
+
+	challenge_1();
+	cin.ignore();
+
+	challenge_2();
+	cin.ignore();
+
+	challenge_3();
+	cin.ignore();
+
+	challenge_4();
+	cin.ignore();
+
+	challenge_5();
+	cin.ignore();
+
+	challenge_6();
+	cin.ignore();
+
+	challenge_7();
+	cin.ignore();
+
+	challenge_8();
+	cin.ignore();
+
+	challenge_9();
+	cin.ignore();
+
+	challenge_10();
+	cin.ignore();
+
+	challenge_11();
+	cin.ignore();
+
+	challenge_12();
+	cin.ignore();
+
+	challenge_13();
+	cin.ignore();
+
+	challenge_14();
+	cin.ignore();
+
+	challenge_15();
+	cin.ignore();
+
+	return 0;
+}
